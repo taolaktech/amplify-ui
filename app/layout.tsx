f0fa2170f8a1e917a84e1235c7ca0df41ee1b453
den-ii <wisdomochiche@gmail.com>
2025-03-28 21:55:31 +0100

    session loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SessionLoader from "./ui/loaders/SessionLoader";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <div>{children}</div>
+        <SessionLoader />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,7 @@ import Image from "next/image";
 import Logo from "./ui/loaders/SessionLoader";
 
 export default function Home() {
-  return <Logo />;
+  return (
+    <h1 className="flex font-bold text-4xl justify-center">Helllllllll</h1>
+  );
 }
