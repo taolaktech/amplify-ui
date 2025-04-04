b3eae5f236a48455bd3f86bba48450dbe4aaf0b4
den-ii <wisdomochiche@gmail.com>
2025-04-04 13:03:42 +0100

    fix: navbar alignment

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -5,7 +5,7 @@ import LogoSMIcon from "@/public/nav-logo-sm.svg";
 export default function Navbar() {
   return (
     <nav className="fixed top-0 w-full bg-[#FBFAFC] md:bg-white flex items-center h-navbar-height z-10 container-base-container px-base-padding py-3">
-      <div className="max-w-[1512px] mx-auto w-full">
+      <div className="max-w-[1512px] mx-auto w-full flex items-center">
         <Link href={"/"} className="hidden md:inline-block">
           <LogoIcon width={109} height={32} />
         </Link>
