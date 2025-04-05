dfb167c4a3fb31b9d03a77373ef0ded7b10fa8dc
taolak <taolak4orcl@gmail.com>
2025-04-04 23:37:48 -0500

    remove logging

--- app/signup/create/verify-account/page.tsx
+++ app/signup/create/verify-account/page.tsx
@@ -56,7 +56,6 @@ export default function VerifyAccount() {
       }
     } else if (/^\d$/.test(e.key)) {
       if (codes[index] !== "" && Number(codes[index]) >= 0 && index < 5) {
-        console.log("yeah");
         codeRefs.current[index + 1]?.focus();
         setCode((code) => {
           code[index + 1] = e.key;
