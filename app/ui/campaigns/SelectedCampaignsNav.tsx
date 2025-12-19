03ef281b98910ed4fab87b32d4199d0677ae83ce
Deni <wisdomochiche@gmail.com>
2025-12-19 17:25:14 +0100

    switch to change budget

--- app/ui/campaigns/SelectedCampaignsNav.tsx
+++ app/ui/campaigns/SelectedCampaignsNav.tsx
@@ -1,5 +1,11 @@
 import useCampaignsStore from "@/app/lib/stores/campaignsStore";
-import { CloseCircle, Edit2, PauseCircle } from "iconsax-react";
+import {
+  CloseCircle,
+  Edit2,
+  EmptyWalletChange,
+  PauseCircle,
+  WalletAdd,
+} from "iconsax-react";
 import { useMemo } from "react";
 
 export default function SelectedCampaignsNav() {
@@ -40,13 +46,14 @@ export default function SelectedCampaignsNav() {
             </div>
             <div className="flex gap-2 items-center">
               <button className="w-[72px] h-[34px] rounded-xl bg-[rgba(255,255,255,0.10)] flex items-center justify-center gap-1">
-                <Edit2 size={12} color="#FFF" />
-                <span className="text-white text-sm">Edit</span>
-              </button>
-              <button className="w-[72px] h-[34px] rounded-xl bg-[rgba(255,255,255,0.10)] flex items-center justify-center gap-1">
                 <PauseCircle size={12} color="#FFF" />
                 <span className="text-white text-sm">Pause</span>
               </button>
+              <button className="w-[137px] h-[34px] rounded-xl bg-[rgba(255,255,255,0.10)] flex items-center justify-center gap-1">
+                <EmptyWalletChange size={17.5} color="#FFF" />
+                <span className="text-white text-sm">Change Budget</span>
+              </button>
+
               <button className="w-[79px] h-[34px] rounded-xl bg-[#FF4949] flex items-center justify-center gap-1">
                 <PauseCircle size={12} color="#FFF" />
                 <span className="text-white text-sm">Archive</span>
--- app/ui/campaigns/Settings.tsx
+++ app/ui/campaigns/Settings.tsx
@@ -1,4 +1,11 @@
-import { Archive, CopySuccess, PauseCircle, Play } from "iconsax-react";
+import {
+  Archive,
+  Coin1,
+  CopySuccess,
+  EmptyWalletChange,
+  PauseCircle,
+  Play,
+} from "iconsax-react";
 
 export function ProductOptions({
   dropdownPosition,
@@ -11,8 +18,10 @@ export function ProductOptions({
        w-[199px] bg-white custom-shadow-sm rounded-[10px] border-[0.5] border-[#B6B3B9]`}
     >
       <div className="h-[52px] hover:bg-[rgba(243,239,246,0.5)] cursor-pointer border-b-[0.25px] border-[#B6B3B9] px-4 gap-2 flex items-center">
-        <Play size={16} color="#1D0B30" />
-        <span className="text-sm font-medium">View Campaign</span>
+        {/* <Play size={16} color="#1D0B30" /> */}
+        {/* <Coin1 size="16" color="#1D0B30" /> */}
+        <EmptyWalletChange size="17.5" color="#1D0B30" />
+        <span className="text-sm font-medium">Change Budget</span>
       </div>
       <div className="h-[52px] hover:bg-[rgba(243,239,246,0.5)] cursor-pointer border-b-[0.25px] border-[#B6B3B9] px-4 gap-2 flex items-center">
         <CopySuccess size={16} color="#1D0B30" />
