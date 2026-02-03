a1ed3a34c5e9b85ce9f84d98772a5f069d856acd
Akinolaaa <akin.akinleye619@gmail.com>
2026-02-03 21:21:10 +0100

    removing unused code

--- amplify-backend/src/modules/auth/decorators/subscription-required.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { SUBSCRIPTION_REQUIRED_KEY } from '../guards/subscription.guard';
-
-export const SubscriptionRequired = () =>
-  SetMetadata(SUBSCRIPTION_REQUIRED_KEY, true);
