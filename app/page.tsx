1bb08166bc4c0ba3932805e817cf303e524be2fe
Akinolaaa <akin.akinleye619@gmail.com>
2026-04-10 14:24:05 +0100

    update: ad library to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import GettingStarted from "./ui/dashboard/GettingStarted";
 import Metrics from "./ui/dashboard/metrics";
+import CreativeTemplates from "./ui/dashboard/CreativeTemplates";
 
 export default function DashboardPage() {
   return (
     <main className="">
       <GettingStarted />
-      <Metrics />
+      <CreativeTemplates />
     </main>
   );
 }
